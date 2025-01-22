601529d962144adfd2d722e7a5ab457ad4bc414e
PinT271 <olezhka.kuzmin.99@inbox.ru>
2025-01-22 12:02:10 +0700

    Добавление роута на регу и создание контроллера

---
+++ example-app/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function registration(Request $request){
+
+    }
+}
