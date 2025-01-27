4032114986a752aef32f12c412f9df02910d31c6
PinT271 <olezhka.kuzmin.99@inbox.ru>
2025-01-27 15:04:55 +0700

    Добавил очереди

--- example-app/config/verification-code.php
+++ example-app/config/verification-code.php
@@ -83,7 +83,7 @@ return [
     | will be used for the notification.
     |
     */
-    'queue' => null,
+    'queue' => 'verificationCode',
 
     /*
     |--------------------------------------------------------------------------
